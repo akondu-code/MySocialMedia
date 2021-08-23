ea656927292564109000d7b9e3e9109877cc431c
akondu-code <akonduako@gmail.com>
2021-08-23 08:16:45 +0100

    final commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-//Declaring variables
-const submitInput = document.getElementById("submitInput");
+const submitInput = document.getElementById("submitInput")
+const tweetBtn = document.getElementById("send-tweet")
  
-submitInput?.addEventListener("click", e => {
+submitInput?.addEventListener("click", () => {
      const userName = document.getElementById("input-userName").value;
     const displayName = document.getElementById("input-displayName").value;
     localStorage.setItem("username", userName)
@@ -9,10 +9,45 @@ submitInput?.addEventListener("click", e => {
     location.replace("home.html")
 })
 
+tweetBtn?.addEventListener("click", () => {
+    const tweetInput = document.getElementById("input-tweet").value
+    localStorage.setItem("tweetValue", tweetInput)
+    location.replace("home.html")
+})
+
 const getUser = localStorage.getItem("username")
 const getDisplay = localStorage.getItem("displayname")
-console.log(getUser)
+const tweetValue = localStorage.getItem("tweetValue")
 
-document.getElementById("greeting").innerHTML=`Hey ${getDisplay} !`
-document.getElementById("display-name").innerHTML= getDisplay
-document.getElementById("User-name").innerHTML= `@ ${getUser}`
+const greetingDiv = document.getElementById("greeting")
+const displayDiv = document.getElementById("display-name")
+const userDiv = document.getElementById("User-name")
+const tweetContainer = document.getElementById("tweet-container")
+
+if(tweetContainer) {
+    tweetContainer.innerHTML += `<div class="card col-12 mb-3">
+    <div class="card-body d-flex flex-column">
+    <div class="d-flex flex-row">
+    <div><img src="img/user-img.jpg" alt="" width="40" height="40" class="rounded-circle"></div>
+    <div class="fw-bold ps-2" id="display-name">
+        ${getDisplay}
+    </div>
+    <div class="ps-2" id="User-name">@${getUser}</div>
+    </div>
+    
+    <div class="ms-5 fw-lighter" id="posted-tweet">
+        ${tweetValue}
+    </div>
+    </div>
+        </div>`
+}
+
+if(greetingDiv) {
+    greetingDiv.innerHTML = `Hey ${getDisplay} !`
+}
+if(displayDiv) {
+    displayDiv.innerHTML = getDisplay
+}
+if(userDiv) {
+    userDiv.innerHTML = `@ ${getUser}`
+}
